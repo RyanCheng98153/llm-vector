963e03c5bf8f04c4c6f00b50ce0b13c9074f9000
ryan98153 <110703007@nccu.edu.tw>
2025-02-20 11:41:42 +0800

    testing

--- test.py
+++ test.py
@@ -291,8 +291,8 @@ class KVCacheModifier:
         
         # Modify the token of cat_kvlen-1 with the delta_key and delta_value
         for i in range(len(full_dog_kv.key_cache)):
-            delta_key_expanded = delta_key.unsqueeze(2).expand_as(full_dog_kv.key_cache[i][:, :, cat_kvlen-1:, :])
-            delta_value_expanded = delta_value.unsqueeze(2).expand_as(full_dog_kv.value_cache[i][:, :, cat_kvlen-1:, :])
+            delta_key_expanded = delta_key.unsqueeze(2).expand_as(full_dog_kv.key_cache[i][:, :, cat_kvlen-1, :])
+            delta_value_expanded = delta_value.unsqueeze(2).expand_as(full_dog_kv.value_cache[i][:, :, cat_kvlen-1, :])
             test_cat_kv.key_cache[i][:, :, cat_kvlen-1:, :] = full_dog_kv.key_cache[i][:, :, cat_kvlen-1:, :] + delta_key_expanded
             test_cat_kv.value_cache[i][:, :, cat_kvlen-1:, :] = full_dog_kv.value_cache[i][:, :, cat_kvlen-1:, :] + delta_value_expanded
             
@@ -304,8 +304,8 @@ class KVCacheModifier:
         
         # Modify the token of cat_kvlen-1 with the delta_key and delta_value
         for i in range(len(full_dog_kv.key_cache)):
-            delta_key_expanded = delta_key.unsqueeze(2).expand_as(full_dog_kv.key_cache[i][:, :, cat_kvlen-1:, :])
-            delta_value_expanded = delta_value.unsqueeze(2).expand_as(full_dog_kv.value_cache[i][:, :, cat_kvlen-1:, :])
+            delta_key_expanded = delta_key.unsqueeze(2).expand_as(full_dog_kv.key_cache[i][:, :, cat_kvlen-1, :])
+            delta_value_expanded = delta_value.unsqueeze(2).expand_as(full_dog_kv.value_cache[i][:, :, cat_kvlen-1, :])
             test_cat_kv.key_cache[i][:, :, cat_kvlen-1:, :] = full_dog_kv.key_cache[i][:, :, cat_kvlen-1:, :] + delta_key_expanded
             test_cat_kv.value_cache[i][:, :, cat_kvlen-1:, :] = full_dog_kv.value_cache[i][:, :, cat_kvlen-1:, :] + delta_value_expanded
         
